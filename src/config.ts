0f3d0f135c3505413385ca6095585662e62ef1f2
Frank Wei <frank@frankwei.xyz>
2021-02-26 10:11:29 +0800

    go test

--- src/config.ts
+++ src/config.ts
@@ -14,8 +14,29 @@ const configurations: { [env: string]: Configuration } = {
       BUSD: ['0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee', 18],
       USDC: ['0x64544969ed7ebf5f083679233325356ebe738930', 18],
       USDT: ['0x337610d27c682e347c9cd60bd4b3b107c9d34ddd', 18],
-      'YSD_BUSD-UNI-LPv2': ['0xbd6e2C9D4827ee3422527556b06be10fb8B79Eb1', 18],
-      'YSS_BUSD-UNI-LPv2': ['0xDfa8AE6Bd5BFc79E98adC1B7f68c2F86eb6a3920', 18],
+      'YSD_BUSD-UNI-LPv2': ['0xD678596Dcb6878ceed88852c9db08A368ec3DD09', 18],
+      'YSS_BUSD-UNI-LPv2': ['0x4B7eB05FA20560C9B152aC4b325485253f6A6009', 18],
+    },
+    baseLaunchDate: new Date('2021-01-26T11:00:00Z'),
+    wholeSiteLaunchesAt: new Date('2021-01-26T11:00:00Z'),
+    bondLaunchesAt: new Date('2021-01-27T00:00:00Z'),
+    boardroomLaunchesAt: new Date('2021-01-02T00:00:00Z'),
+    refreshInterval: 10000,
+    gasLimitMultiplier: 1.1,
+  },
+  production: {
+    chainId: ChainId.BSC_TESTNET,
+    etherscanUrl: 'https://testnet.bscscan.com',
+    defaultProvider: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+    deployments: require('./basis-cash/deployments/deployments.bsc_testnet.json'),
+    externalTokens: {
+      DAI: ['0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867', 18],
+      // yCRV: ['0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', 18],
+      BUSD: ['0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee', 18],
+      USDC: ['0x64544969ed7ebf5f083679233325356ebe738930', 18],
+      USDT: ['0x337610d27c682e347c9cd60bd4b3b107c9d34ddd', 18],
+      'YSD_BUSD-UNI-LPv2': ['0xD678596Dcb6878ceed88852c9db08A368ec3DD09', 18],
+      'YSS_BUSD-UNI-LPv2': ['0x4B7eB05FA20560C9B152aC4b325485253f6A6009', 18],
     },
     baseLaunchDate: new Date('2021-01-26T11:00:00Z'),
     wholeSiteLaunchesAt: new Date('2021-01-26T11:00:00Z'),
@@ -25,34 +46,34 @@ const configurations: { [env: string]: Configuration } = {
     gasLimitMultiplier: 1.1,
   },
   // Just copy from dev for build
-  production: {
-    chainId: ChainId.BSC_TESTNET,
-    etherscanUrl: 'https://testnet.bscscan.com',
-    defaultProvider: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-    deployments: require('./basis-cash/deployments/deployments.bsc_testnet.json'),
-    externalTokens: {
-      DAI: ['0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867', 18],
-      // yCRV: ['0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', 18],
-      BUSD: ['0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18],
-      USDC: ['0x64544969ed7ebf5f083679233325356ebe738930', 18],
-      USDT: ['0x337610d27c682e347c9cd60bd4b3b107c9d34ddd', 18],
-      'YSD_BUSD-UNI-LPv2': ['0x4007bE31d732aCd882D25183919830c98DCa52e1', 18],
-      'YSS_BUSD-UNI-LPv2': ['0x3EDEB4cB126fD4C809Bd2B31cDb86ab0aCcfC099', 18],
-    },
-    // Use this for public preview
-    // baseLaunchDate: new Date('2021-02-26T00:00:00Z'), // TODO
-    // wholeSiteLaunchesAt: new Date('2021-01-11T12:00:00Z'),
-    // bondLaunchesAt: new Date('2021-01-11T12:00:00Z'), // TODO
-    // boardroomLaunchesAt: new Date('2021-01-11T12:00:00Z'), // TODO
+  // production: {
+  //   chainId: ChainId.BSC_TESTNET,
+  //   etherscanUrl: 'https://testnet.bscscan.com',
+  //   defaultProvider: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+  //   deployments: require('./basis-cash/deployments/deployments.bsc_testnet.json'),
+  //   externalTokens: {
+  //     DAI: ['0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867', 18],
+  //     // yCRV: ['0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', 18],
+  //     BUSD: ['0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18],
+  //     USDC: ['0x64544969ed7ebf5f083679233325356ebe738930', 18],
+  //     USDT: ['0x337610d27c682e347c9cd60bd4b3b107c9d34ddd', 18],
+  //     'YSD_BUSD-UNI-LPv2': ['0x4007bE31d732aCd882D25183919830c98DCa52e1', 18],
+  //     'YSS_BUSD-UNI-LPv2': ['0x3EDEB4cB126fD4C809Bd2B31cDb86ab0aCcfC099', 18],
+  //   },
+  //   // Use this for public preview
+  //   // baseLaunchDate: new Date('2021-02-26T00:00:00Z'), // TODO
+  //   // wholeSiteLaunchesAt: new Date('2021-01-11T12:00:00Z'),
+  //   // bondLaunchesAt: new Date('2021-01-11T12:00:00Z'), // TODO
+  //   // boardroomLaunchesAt: new Date('2021-01-11T12:00:00Z'), // TODO
 
-    // Only for internal preview
-    baseLaunchDate: new Date('2021-02-26T11:00:00Z'),
-    wholeSiteLaunchesAt: new Date('2021-02-26T11:00:00Z'),
-    bondLaunchesAt: new Date('2021-02-27T00:00:00Z'),
-    boardroomLaunchesAt: new Date('2021-03-02T00:00:00Z'),
-    refreshInterval: 10000,
-    gasLimitMultiplier: 1.1,
-  },
+  //   // Only for internal preview
+  //   baseLaunchDate: new Date('2021-02-26T11:00:00Z'),
+  //   wholeSiteLaunchesAt: new Date('2021-02-26T11:00:00Z'),
+  //   bondLaunchesAt: new Date('2021-02-27T00:00:00Z'),
+  //   boardroomLaunchesAt: new Date('2021-03-02T00:00:00Z'),
+  //   refreshInterval: 10000,
+  //   gasLimitMultiplier: 1.1,
+  // },
 };
 
 export const bankDefinitions: { [contractName: string]: BankInfo } = {
