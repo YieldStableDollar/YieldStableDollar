01860719ce0ef8eba43511dfcd2ac7ff588e5d8b
Frank Wei <frank@frankwei.xyz>
2021-02-28 23:10:09 +0800

    fix: Chinese warning

--- src/components/BetaWarning.tsx
+++ src/components/BetaWarning.tsx
@@ -4,13 +4,16 @@ import styled from 'styled-components';
 const StyledBetaWarning = styled.div`
 background: #fbdf15;
 border-radius: 2px;
+font-size: 20px;
 border-color: #a9a924;
 `;
 
 const BetaWarning = () => <StyledBetaWarning className="beta-warning">
-        <h2 className="title"> Do Not YOLO a BETA version </h2>
+        <h2 className="title"> Do Not YOLO a BETA version 这是一个测试版本 </h2>
         <p>By Pressing the "Unlock Wallet" button, you're aware of that YSD is currently in <b>BETA</b>.</p>
         <p> DO NOT deposit LARGE amounts of Coins as bugs are unknown. </p>
+        <p> <b>请不要充值大额资产，不要提供大额的流动性。这是一个测试版本。</b></p>
+        <p> 提供流动性会有损失风险，请慎重考虑。</p>
 </StyledBetaWarning>
 
 
